e9e9f1f9481c698334bc83fc1a40e3315471c565
Mario Dominguez <mariodominguez@eprosima.com>
2024-02-02 11:01:11 +0100

    Refs #19827: Orchestrator implementation
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

---
+++ sustainml_cpp/src/cpp/orchestrator/ModuleNodeProxy.cpp
@@ -0,0 +1,241 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file ModuleNodeProxy.cpp
+ */
+
+
+#include <orchestrator/ModuleNodeProxy.hpp>
+
+#include "TaskDB.ipp"
+
+#include <common/Common.hpp>
+
+namespace sustainml {
+namespace orchestrator {
+
+ModuleNodeProxy::ModuleNodeProxyListener::ModuleNodeProxyListener(ModuleNodeProxy* proxy) :
+    proxy_parent_(proxy)
+{
+
+}
+
+void ModuleNodeProxy::ModuleNodeProxyListener::on_data_available(
+    eprosima::fastdds::dds::DataReader* reader)
+{
+    eprosima::fastdds::dds::SampleInfo info;
+
+    void* tmp_untyped_impl_data = proxy_parent_->get_tmp_impl_typed_data();
+    if (reader->take_next_sample(tmp_untyped_impl_data, &info) == eprosima::fastrtps::types::ReturnCode_t::RETCODE_OK)
+    {
+        if (info.instance_state == eprosima::fastdds::dds::ALIVE_INSTANCE_STATE)
+        {
+            EPROSIMA_LOG_INFO(MODULE_PROXY, "notify_new_node_ouput " << proxy_parent_->name_);
+            proxy_parent_->notify_new_node_ouput(tmp_untyped_impl_data);
+        }
+    }
+}
+
+void ModuleNodeProxy::ModuleNodeProxyListener::on_subscription_matched(
+    eprosima::fastdds::dds::DataReader* reader,
+    const eprosima::fastdds::dds::SubscriptionMatchedStatus & status)
+{
+
+}
+
+ModuleNodeProxy::ModuleNodeProxyStatusListener::ModuleNodeProxyStatusListener(ModuleNodeProxy* proxy) :
+    proxy_parent_(proxy)
+{
+
+}
+
+void ModuleNodeProxy::ModuleNodeProxyStatusListener::on_data_available(
+    eprosima::fastdds::dds::DataReader* reader)
+{
+    SampleInfo info;
+    // Take next sample from DataReader's history
+    if (ReturnCode_t::RETCODE_OK == reader->take_next_sample(proxy_parent_->status_.get_impl(), &info))
+    {
+        // Some samples only update the instance state. Only if it is a valid sample (with data)
+        if (ALIVE_INSTANCE_STATE == info.instance_state)
+        {
+            // Print structure data
+            EPROSIMA_LOG_INFO(MODULE_PROXY, "New Status " << proxy_parent_->status_.node_name() << " " << proxy_parent_->status_.node_status() << " RECEIVED");
+            proxy_parent_->notify_status_change();
+        }
+    }
+}
+
+void ModuleNodeProxy::ModuleNodeProxyStatusListener::on_subscription_matched(
+    eprosima::fastdds::dds::DataReader*,
+    const eprosima::fastdds::dds::SubscriptionMatchedStatus & status)
+{
+
+}
+
+ModuleNodeProxy::ModuleNodeProxy(
+    OrchestratorNode* orchestrator,
+    std::shared_ptr<TaskDB_t> task_db,
+    const char * name) :
+    name_(name),
+    node_id_(common::get_node_id_from_name(name_)),
+    orchestrator_(orchestrator),
+    task_db_(task_db),
+    listener_(this),
+    status_listener_(this)
+{
+    if (orchestrator_->participant_ == nullptr ||
+        orchestrator_->sub_ == nullptr ||
+        orchestrator_->status_topic_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR_NODE_PROXY, "Error construction");
+        return;
+    }
+
+    status_.node_name(name);
+
+    node_output_topic_ = orchestrator_->participant_->create_topic(
+        common::TopicCollection::get()[common::get_topic_from_name(name)].first.c_str(),
+        common::TopicCollection::get()[common::get_topic_from_name(name)].second.c_str(), TOPIC_QOS_DEFAULT);
+
+    if (node_output_topic_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR_NODE_PROXY, "Error creating the status topic in " << name);
+        return;
+    }
+
+    DataReaderQos drqos;
+    drqos.durability().kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    drqos.reliability().kind = RELIABLE_RELIABILITY_QOS;
+
+    node_output_datareader_ = orchestrator_->sub_->create_datareader(node_output_topic_, drqos, &listener_);
+
+    std::string expression("node_name like %0");
+    std::vector<std::string> parameters;
+    parameters.push_back(std::string("'") + name + "'");
+
+    filtered_status_topic_ = orchestrator_->participant_->create_contentfilteredtopic(
+        (common::TopicCollection::get()[common::Topics::NODE_STATUS].first + "_" + name).c_str(),
+        orchestrator_->status_topic_,
+        expression,
+        parameters);
+
+    status_datareader_ = orchestrator_->sub_->create_datareader(filtered_status_topic_, drqos, &status_listener_);
+
+    if (status_datareader_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR_NODE_PROXY, "Error creating the status datareader in " << name);
+        return;
+    }
+}
+
+ModuleNodeProxy::~ModuleNodeProxy()
+{
+    if (status_datareader_)
+    {
+        status_datareader_->set_listener(nullptr);
+    }
+
+    if (node_output_datareader_)
+    {
+        node_output_datareader_->set_listener(nullptr);
+    }
+}
+
+void ModuleNodeProxy::notify_status_change()
+{
+    std::lock_guard<std::mutex> lock(orchestrator_->mtx_);
+    std::shared_ptr<OrchestratorNodeHandle> handler_ptr = orchestrator_->get_handler().lock();
+    if (handler_ptr != nullptr)
+    {
+        handler_ptr->on_node_status_change(node_id_, status_);
+    }
+}
+
+void ModuleNodeProxy::notify_new_node_ouput(void* data)
+{
+    store_data_in_db();
+    std::lock_guard<std::mutex> lock(orchestrator_->mtx_);
+    std::shared_ptr<OrchestratorNodeHandle> handler_ptr = orchestrator_->get_handler().lock();
+    if (handler_ptr != nullptr)
+    {
+        handler_ptr->on_new_node_output(node_id_, data);
+    }
+}
+
+void ModuleNodeProxy::set_status(const types::NodeStatus& status)
+{
+    status_ = status;
+}
+
+const types::NodeStatus& ModuleNodeProxy::get_status()
+{
+    return status_;
+}
+
+TaskEncoderNodeProxy::TaskEncoderNodeProxy(
+        OrchestratorNode* orchestrator,
+        std::shared_ptr<orchestrator::OrchestratorNode::TaskDB_t> task_db)
+        : ModuleNodeProxy(orchestrator, task_db, common::TASK_ENCODER_NODE)
+{
+
+}
+
+void TaskEncoderNodeProxy::store_data_in_db()
+{
+    task_db_->insert_task_data(tmp_data_.task_id(), tmp_data_);
+}
+
+MLModelProviderNodeProxy::MLModelProviderNodeProxy(
+        OrchestratorNode* orchestrator,
+        std::shared_ptr<orchestrator::OrchestratorNode::TaskDB_t> task_db)
+        : ModuleNodeProxy(orchestrator, task_db, common::ML_MODEL_NODE)
+{
+
+}
+
+void MLModelProviderNodeProxy::store_data_in_db()
+{
+    task_db_->insert_task_data(tmp_data_.task_id(), tmp_data_);
+}
+
+HardwareResourcesProviderNodeProxy::HardwareResourcesProviderNodeProxy(
+        OrchestratorNode* orchestrator,
+        std::shared_ptr<orchestrator::OrchestratorNode::TaskDB_t> task_db)
+        : ModuleNodeProxy(orchestrator, task_db, common::HW_RESOURCES_NODE)
+{
+
+}
+
+void HardwareResourcesProviderNodeProxy::store_data_in_db()
+{
+    task_db_->insert_task_data(tmp_data_.task_id(), tmp_data_);
+}
+
+CarbonFootprintProviderNodeProxy::CarbonFootprintProviderNodeProxy(
+        OrchestratorNode* orchestrator,
+        std::shared_ptr<orchestrator::OrchestratorNode::TaskDB_t> task_db)
+        : ModuleNodeProxy(orchestrator, task_db, common::CO2_TRACKER_NODE)
+{
+
+}
+
+void CarbonFootprintProviderNodeProxy::store_data_in_db()
+{
+    task_db_->insert_task_data(tmp_data_.task_id(), tmp_data_);
+}
+
+} // namespace orchestrator
+} // namespace sustainml
---
+++ sustainml_cpp/src/cpp/orchestrator/OrchestratorNode.cpp
@@ -0,0 +1,324 @@
+
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file OrchestratorNode.cpp
+ */
+
+#include <sustainml_cpp/orchestrator/OrchestratorNode.hpp>
+
+#include "ModuleNodeProxyFactory.hpp"
+#include "TaskDB.ipp"
+
+#include <common/Common.hpp>
+#include <orchestrator/TaskManager.hpp>
+#include <types/typesImplPubSubTypes.h>
+#include <types/typesImplTypeObject.h>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/log/Log.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+using namespace eprosima::fastdds::dds;
+
+namespace sustainml {
+namespace orchestrator {
+
+OrchestratorNode::OrchestratorParticipantListener::OrchestratorParticipantListener(
+    OrchestratorNode* orchestrator) :
+    orchestrator_(orchestrator)
+{
+
+}
+
+void OrchestratorNode::OrchestratorParticipantListener::on_participant_discovery(
+    eprosima::fastdds::dds::DomainParticipant* participant,
+    eprosima::fastrtps::rtps::ParticipantDiscoveryInfo&& info)
+{
+    eprosima::fastrtps::string_255 participant_name = info.info.m_participantName;
+    EPROSIMA_LOG_INFO(ORCHESTRATOR, "Orchestrator discovered a new Participant with name " << participant_name);
+
+    // Synchronise with Orchestrator initialization
+    if (!orchestrator_->initialized_.load())
+    {
+        std::unique_lock<std::mutex> lock(orchestrator_->get_mutex());
+        orchestrator_->initialization_cv_.wait(lock, [&](){
+            return orchestrator_->initialized_.load();
+        });
+    }
+
+    // create the proxy for this node
+    NodeID node_id = common::get_node_id_from_name(participant_name);
+
+    if (info.status == info.DISCOVERED_PARTICIPANT &&
+        orchestrator_->node_proxies_[static_cast<uint32_t>(node_id)] == nullptr)
+    {
+        EPROSIMA_LOG_INFO(ORCHESTRATOR, "Creating node proxy for " << participant_name << " node");
+        ModuleNodeProxyFactory::make_node_proxy(
+            node_id,
+            orchestrator_,
+            orchestrator_->task_db_,
+            orchestrator_->node_proxies_[static_cast<uint32_t>(node_id)]);
+    }
+    else if ((info.status == info.DROPPED_PARTICIPANT || info.status == info.REMOVED_PARTICIPANT) &&
+            orchestrator_->node_proxies_[static_cast<uint32_t>(node_id)] != nullptr)
+    {
+        EPROSIMA_LOG_INFO(ORCHESTRATOR, "Setting inactive " << participant_name << " node");
+        types::NodeStatus status = orchestrator_->node_proxies_[static_cast<uint32_t>(node_id)]->get_status();
+        status.node_status(NODE_INACTIVE);
+        std::lock_guard<std::mutex> lock(orchestrator_->mtx_);
+        orchestrator_->node_proxies_[static_cast<uint32_t>(node_id)]->set_status(status);
+    }
+}
+
+OrchestratorNode::OrchestratorNode(std::shared_ptr<OrchestratorNodeHandle> handle, uint32_t domain) :
+    domain_(domain),
+    handler_(handle),
+    node_proxies_({
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr
+    }),
+    task_db_(new TaskDB_t()),
+    task_man_(new TaskManager()),
+    participant_listener_(new OrchestratorParticipantListener(this))
+{
+    if (!init())
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Orchestrator initialization Failed");
+    }
+}
+
+OrchestratorNode::~OrchestratorNode()
+{
+
+    for (size_t i = 0; i < (size_t)NodeID::MAX; i++)
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        if (node_proxies_[i] != nullptr)
+        {
+            delete node_proxies_[i];
+        }
+    }
+
+    if (sub_ != nullptr)
+    {
+        sub_->delete_contained_entities();
+    }
+
+    if (pub_ != nullptr)
+    {
+        pub_->delete_contained_entities();
+    }
+
+    if (participant_ != nullptr)
+    {
+        participant_->delete_contained_entities();
+    }
+
+    DomainParticipantFactory::get_instance()->delete_participant(participant_);
+
+    delete task_man_;
+}
+
+bool OrchestratorNode::init()
+{
+    auto dpf = DomainParticipantFactory::get_instance();
+
+    //! Initialize entities
+    participant_ = dpf->create_participant(domain_,
+        PARTICIPANT_QOS_DEFAULT,
+        participant_listener_.get(),
+        StatusMask::all() >> StatusMask::data_on_readers());
+
+    if (participant_ == nullptr)
+    {
+        return false;
+    }
+
+    //! Register Common Types
+    std::vector<eprosima::fastdds::dds::TypeSupport> sustainml_types;
+    sustainml_types.reserve(common::Topics::MAX);
+
+    sustainml_types.push_back(static_cast<TypeSupport>(new NodeStatusImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new NodeControlImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new UserInputImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new EncodedTaskImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new MLModelImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new HWResourceImplPubSubType()));
+    sustainml_types.push_back(static_cast<TypeSupport>(new CO2FootprintImplPubSubType()));
+
+    for (auto&& type : sustainml_types)
+    {
+        participant_->register_type(type);
+    }
+
+    // Register type objects
+    registertypesImplTypes();
+
+    status_topic_ = participant_->create_topic(common::TopicCollection::get()[common::Topics::NODE_STATUS].first.c_str(),
+        common::TopicCollection::get()[common::Topics::NODE_STATUS].second.c_str(), TOPIC_QOS_DEFAULT);
+
+    control_topic_ = participant_->create_topic(common::TopicCollection::get()[common::Topics::NODE_CONTROL].first.c_str(),
+        common::TopicCollection::get()[common::Topics::NODE_CONTROL].second.c_str(), TOPIC_QOS_DEFAULT);
+
+    user_input_topic_ = participant_->create_topic(common::TopicCollection::get()[common::Topics::USER_INPUT].first.c_str(),
+        common::TopicCollection::get()[common::Topics::USER_INPUT].second.c_str(), TOPIC_QOS_DEFAULT);
+
+    if (status_topic_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Error creating the status topic");
+        return false;
+    }
+
+    pub_ = participant_->create_publisher(PUBLISHER_QOS_DEFAULT);
+
+    if (pub_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Error creating the publisher");
+        return false;
+    }
+
+    control_writer_ = pub_->create_datawriter(control_topic_, DATAWRITER_QOS_DEFAULT);
+
+    if (control_writer_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Error creating the control writer");
+        return false;
+    }
+
+    user_input_writer_ = pub_->create_datawriter(user_input_topic_, DATAWRITER_QOS_DEFAULT);
+
+    if (user_input_topic_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Error creating the user input writer");
+        return false;
+    }
+
+    sub_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+
+    if (sub_ == nullptr)
+    {
+        EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Error creating the subscriber");
+        return false;
+    }
+
+    initialized_.store(true);
+    initialization_cv_.notify_one();
+    return true;
+}
+
+
+std::pair<int, types::UserInput*> OrchestratorNode::prepare_new_task()
+{
+    std::pair<int, types::UserInput*> output;
+    int new_task_id = task_man_->create_new_task_id();
+    task_db_->prepare_new_entry(new_task_id);
+    task_db_->get_task_data(new_task_id, output.second);
+    output.first = new_task_id;
+    return output;
+}
+
+bool OrchestratorNode::start_task(const int &task_id, types::UserInput* ui)
+{
+    user_input_writer_->write(ui->get_impl());
+    return true;
+}
+
+RetCode_t OrchestratorNode::get_task_data(const int &task_id, const NodeID& node_id, void*& data)
+{
+    RetCode_t ret = RetCode_t::RETCODE_NO_DATA;
+
+    switch (node_id)
+    {
+        case NodeID::ID_TASK_ENCODER:
+        {
+            MapFromNodeIDToType_t<NodeID::ID_TASK_ENCODER>::type* typed_data = nullptr;
+            if (task_db_->get_task_data(task_id, typed_data))
+            {
+                data = typed_data;
+                ret = RetCode_t::RETCODE_OK;
+            }
+            break;
+        }
+        case NodeID::ID_MACHINE_LEARNING:
+        {
+            MapFromNodeIDToType_t<NodeID::ID_MACHINE_LEARNING>::type* typed_data = nullptr;
+            if (task_db_->get_task_data(task_id, typed_data))
+            {
+                data = typed_data;
+                ret = RetCode_t::RETCODE_OK;
+            }
+            break;
+        }
+        case NodeID::ID_HARDWARE_RESOURCES:
+        {
+            MapFromNodeIDToType_t<NodeID::ID_HARDWARE_RESOURCES>::type* typed_data = nullptr;
+            if (task_db_->get_task_data(task_id, typed_data))
+            {
+                data = typed_data;
+                ret = RetCode_t::RETCODE_OK;
+            }
+            break;
+        }
+        case NodeID::ID_CARBON_FOOTPRINT:
+        {
+            MapFromNodeIDToType_t<NodeID::ID_CARBON_FOOTPRINT>::type* typed_data = nullptr;
+            if (task_db_->get_task_data(task_id, typed_data))
+            {
+                data = typed_data;
+                ret = RetCode_t::RETCODE_OK;
+            }
+            break;
+        }
+        case NodeID::ID_ORCHESTRATOR:
+        {
+            MapFromNodeIDToType_t<NodeID::ID_ORCHESTRATOR>::type* typed_data = nullptr;
+            if (task_db_->get_task_data(task_id, typed_data))
+            {
+                data = typed_data;
+                ret = RetCode_t::RETCODE_OK;
+            }
+        }
+        default:
+        {
+            EPROSIMA_LOG_ERROR(ORCHESTRATOR, "Requested Data from non-existing node ID");
+            break;
+        }
+    }
+
+    return ret;
+}
+
+RetCode_t OrchestratorNode::get_node_status(const NodeID &node_id, const types::NodeStatus* &status)
+{
+    RetCode_t ret = RetCode_t::RETCODE_NO_DATA;
+
+    if ((int)node_id >= 0 && node_id < NodeID::MAX)
+    {
+        status = &node_proxies_[(int)node_id]->get_status();
+        ret = RetCode_t::RETCODE_OK;
+    }
+
+    return ret;
+}
+
+} // namespace orchestrator
+} // namespace orchestrator
+
