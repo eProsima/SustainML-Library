972c1364aaa9b3ac4b5f51d87f853d9ced4e9e5b
eProsima <jesuspoderoso@eprosima.com>
2024-10-08 10:00:57 +0200

    Refs #21816: Update back-end node to create a Flask server with the orchestrator
    Signed-off-by: eProsima <jesuspoderoso@eprosima.com>

--- sustainml_modules/sustainml_modules/sustainml-wp5/backend_node.py
+++ sustainml_modules/sustainml_modules/sustainml-wp5/backend_node.py
@@ -13,51 +13,82 @@
 # limitations under the License.
 """SustainML Backend Node Implementation."""
 
-from sustainml_py.nodes.OrchestratorNode import OrchestratorNode
-
-# Manage signaling
-import signal
+from flask import Flask, request
 import threading
 import time
+import signal
+import sys
+from orchestrator_node import orchestrator_node
+from werkzeug.serving import make_server
 
-# Whether to go on spinning or interrupt
-running = False
+running = True
+orchestrator = orchestrator_node.Orchestrator()
+server = Flask(__name__)
+server_ip_address = '127.0.0.1'
+server_port = 5000
 
-# Signal handler
+# Flask server default route
+@server.route('/')
+def hello_world():
+    return 'Hello world! Use "/terminate" route to stop Back-end node.\n'
+
+@server.route('/status', methods=['GET'])
+def status():
+    return orchestrator.get_all_status()
+
+# Flask server shutdown route
+@server.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_func = request.environ.get('werkzeug.server.shutdown')
+    if shutdown_func is None:
+        return 'Use "/terminate" route to stop Back-end node.\n'
+    shutdown_func()
+    return 'Terminating...\n'
+
+# Flask server terminate route
+@server.route('/terminate', methods=['GET'])
+def terminate():
+    signal.raise_signal(signal.SIGINT)
+    return 'Terminating...\n'
+
+class ServerThread(threading.Thread):
+    def __init__(self):
+        # Create orchestrator node
+        self.orchestrator_thread = threading.Thread(target=orchestrator.run)
+        # Create Flask server
+        threading.Thread.__init__(self)
+        self.srv = make_server(server_ip_address, server_port, server)
+        self.ctx = server.app_context()
+        self.ctx.push()
+
+    def run(self):
+        # Start the orchestrator thread
+        self.orchestrator_thread.start()
+        # Start the Flask server
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        # Terminate the orchestrator and its thread
+        orchestrator.terminate()
+        # Terminate the Flask server
+        self.srv.shutdown()
+        # Wait for the orchestrator thread to finish
+        self.orchestrator_thread.join()
+
+# Process signal handler
 def signal_handler(sig, frame):
-    print("\nExiting")
-    OrchestratorNode.terminate()
+    print("\nBack-end node terminating...")
+    flask_server_thread.shutdown()
     global running
     running = False
 
-def new_output(id, data):
-    print("New output")
-
-def status_change(id, status):
-    # NodeID_ID_APP_REQUIREMENTS are the kind of ids
-    print("Status change")
-
-# Main workflow routine
-def run():
-    global running
-    running = True
-
-    node = OrchestratorNode(
-        new_node_output=new_output,
-        node_status_change=status_change)
-    node.spin()
-
-# Call main in program execution
+# Main program execution
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
-
-    """Python does not process signals async if
-    the main thread is blocked (spin()) so, tun
-    user work flow in another thread """
-    runner = threading.Thread(target=run)
-    runner.start()
-
+    print("Back-end Node running, use Ctrl+C to terminate. Server listening at http://" + server_ip_address + ":" + str(server_port) + "/")
+    flask_server_thread = ServerThread()
+    flask_server_thread.start()  # Start the Flask server with the orchestrator
     while running:
         time.sleep(1)
-
-    runner.join()
+    flask_server_thread.join()
+    sys.exit(0)
